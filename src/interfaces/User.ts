1cfa58272fd846fefb9a10e44b84b96ec9b85fba
kubo-hide-kun <hide44041@gmail.com>
2021-02-18 17:56:08 +0900

    add: ユーザー情報のインターフェースの作成

---
+++ src/interfaces/User.ts
@@ -0,0 +1,31 @@
+export interface IActivityLog {
+  actionType: "ACCESS_LINK" | "COMPLETE_TASK" | "LOGIN" | "LOGOUT";
+  targetLink: string | null;
+  timestamp: string;
+}
+
+export interface IUser {
+  uid: string;
+  activityLog: IActivityLog[];
+  birthData: string;
+  completionList: string[];
+  displayName: string;
+  email: string;
+  role: "ADMIN" | "MEMBER" | "WAITING_AUTHENTICATION";
+  team:
+    | "WEB_CREATE"
+    | "WEB_DESIGN"
+    | "GAME_CREATE"
+    | "GAME_DESIGN"
+    | "SECURITY"
+    | "AI"
+    | "UNDECIDED";
+}
+
+export const instanceOfUser = (arg: any): arg is IUser =>
+  "uid" in arg &&
+  "birthData" in arg &&
+  "completionList" in arg &&
+  "displayName" in arg &&
+  "email" in arg &&
+  "team" in arg;
