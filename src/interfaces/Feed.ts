a2796137f2830e6ec2600f9101bc762360275a6a
kubo-hide-kun <hide44041@gmail.com>
2021-02-21 22:57:57 +0900

    add: フィード情報のインターフェースの実装

---
+++ src/interfaces/Feed.ts
@@ -0,0 +1,10 @@
+export interface IFeed {
+  title: string;
+  link: string;
+  tags: string[];
+  contributorName: string;
+  timestamp: string;
+  visitors: string[];
+  fans: string[];
+  collectors: string[];
+}
