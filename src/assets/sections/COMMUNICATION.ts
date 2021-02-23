05ea58c6087cdc3a9498c66aa4360922f3299a68
kubo-hide-kun <hide44041@gmail.com>
2021-02-23 16:15:31 +0900

    lesson: コミュニケーション

---
+++ src/assets/sections/COMMUNICATION.ts
@@ -0,0 +1,202 @@
+import { ISection } from ".";
+
+export const COMMUNICATION: ISection = {
+  title: "コミュニケーション",
+  description:
+    "ハッカソンやアプリコンテスト、高専祭などでの企画出しの際や定期的な定例会議の際に" +
+    "効率よく議論を進めていくためのテクニックをまとめました。",
+  contentsList: [
+    {
+      title: "100％好かれる話し方がある",
+      link: "https://youtu.be/3E2omi55sa0",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 32,
+    },
+    {
+      title: "自己紹介と悩み相談でもう悩まない",
+      link: "https://youtu.be/DLtXJmX0joE",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 29,
+    },
+    {
+      title: "一流の雑談力①",
+      link: "https://youtu.be/-SMVyQAu8XM",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 32,
+    },
+    {
+      title: "一流の雑談力②",
+      link: "https://youtu.be/623EFss_x-8",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 30,
+    },
+    {
+      title: "第１回：オリエンテーション",
+      link: "https://youtu.be/SmnHAU8ZAhQ",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 21,
+    },
+    {
+      title: "第2回：印象操作",
+      link: "https://youtu.be/623EFss_x-8",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 19,
+    },
+    {
+      title: "第2回：印象操作",
+      link: "https://youtu.be/MZnKAtuwm_8",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 14,
+    },
+    {
+      title: "第3回：自己開示",
+      link: "https://youtu.be/uknzMa5COgs",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 18,
+    },
+    {
+      title: "第4回：質問力",
+      link: "https://youtu.be/pR6F1AMjdeA",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 14,
+    },
+    {
+      title: "第5回：無限ループ話法",
+      link: "https://youtu.be/w9-4iC05wf8",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 18,
+    },
+    {
+      title: "第6回：PREP（プレップ）法",
+      link: "https://youtu.be/M6BnZMn2s7Y",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 13,
+    },
+    {
+      title: "第7回：強化型PREP（プレップ）法",
+      link: "https://youtu.be/DsIugWF5mHE",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 30,
+    },
+    {
+      title: "第7回：強化型PREP（プレップ）法",
+      link: "https://youtu.be/DsIugWF5mHE",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 30,
+    },
+    {
+      title: "誰でも簡単!アドラー式 傾聴テクニック5選",
+      link: "https://youtu.be/qUxW68w7u0U",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 11,
+    },
+    {
+      title: "成功している人は、なぜ聞き方がうまいのか？",
+      link: "https://youtu.be/NhsgHpd1J8Y",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 14,
+    },
+    {
+      title: "心理的安全性のつくりかた",
+      link: "https://youtu.be/M2V-rX9Mh3E",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 10,
+    },
+    {
+      title: "悪口が損をしてしまう理由を徹底解説【←自発的特徴変換】",
+      link: "https://youtu.be/eBW4NN0YFoA",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 19,
+    },
+    {
+      title: "悪口は言うのも聞くのも危険",
+      link: "https://youtu.be/9EFfIXcu87E",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 5,
+    },
+    {
+      title: "組織が広がり続ける仕組み【ABCで人生楽勝】",
+      link: "https://youtu.be/QJv-1z68tok",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 20,
+    },
+    {
+      title: "頭が悪い人の話し方TOP5",
+      link: "https://youtu.be/JoQ-r20jumc",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 17,
+    },
+    {
+      title: "確実に嫌われる話し方TOP10",
+      link: "https://youtu.be/zE3wfLue4Ag",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 22,
+    },
+    {
+      title: "【対人関係①】カリスマや愛される人は後天的に作れる",
+      link: "https://youtu.be/GhF1_tz0Nrc",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 28,
+    },
+    {
+      title: "【対人関係②】愛され力は笑顔とボディーランゲージで決まる",
+      link: "https://youtu.be/D39dw3VJvX4",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 30,
+    },
+    {
+      title: "相手の名前を一瞬で覚える方法",
+      link: "https://youtu.be/Y5KiFUVp820",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 6,
+    },
+    {
+      title: "NLP式｜ラポールを形成する４つの心理テクニック",
+      link: "https://youtu.be/PJcJXvDoKI0",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 18,
+    },
+    {
+      title: "人に好かれる意外な会話術",
+      link: "https://youtu.be/TwSqlMS0ASg",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 9,
+    },
+    {
+      title: "信頼レベルを爆上げする心理学",
+      link: "https://youtu.be/U8_D55JM2OI",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 20,
+    },
+    {
+      title: "大衆扇動に使われるちょっと怖い心理学",
+      link: "https://youtu.be/FtHsWcGjdzA",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 22,
+    },
+    {
+      title: "誰でも簡単に出来るアイスブレイクのコツ",
+      link: "https://youtu.be/20NjHDLplPA",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 9,
+    },
+    {
+      title: "信頼関係のサインとは？",
+      link: "https://youtu.be/QJv-1z68tok",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 15,
+    },
+    {
+      title: "部下から尊敬される上司と嫌われる上司の違い５選",
+      link: "https://youtu.be/8KKMQtWesYw",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 17,
+    },
+    {
+      title: "「ついて行きたい」と思われる行動ベスト３",
+      link: "https://youtu.be/_AXutf8aOC0",
+      mediaType: ["READ_ONLY", "VIDEO"],
+      requiredTime: 10,
+    },
+  ],
+};
