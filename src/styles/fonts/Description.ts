6461495913bb01cc25e65460ca2e3d8e3e72d31a
kubo-hide-kun <hide44041@gmail.com>
2021-02-23 15:26:06 +0900

    update: コードのリファクタ

--- src/styles/fonts/Description.ts
+++ src/styles/fonts/Description.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Description = styled.p`
-  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo, "MS PGothic", sans-serif;
+  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo,
+    "MS PGothic", sans-serif;
   font-size: 1rem;
   color: rgb(88, 118, 163);
   line-height: 1.8;
-  letter-spacing: .04em;
-  text-decoration: none;
+  letter-spacing: 0.04em;
   margin-bottom: 0;
-`
+`;
