9a29b8ce6948729f92fc89cb0e7faca026611ef5
平田海輝 <seaglory2004@gmail.com>
2021-03-08 10:06:09 +0900

    lesson: VTuber自作講座の作成

---
+++ src/assets/sections/VTUBER.ts
@@ -0,0 +1,64 @@
+import { ISection } from ".";
+
+export const VTUBER: ISection = {
+  title: "バーチャルYouTuberの作り方",
+  description:
+    "Live2D・facing・Vroid・3tene\n" +
+    "Live2Dモデルの作成からFaceRigで動かすまで、初心者にも分かりやすく説明してくれます！",
+  contentsList: [
+    {
+      title: "バ美肉とは？仕組みとツール",
+      link: "https://youtu.be/qBsOTdbchrU",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 14,
+    },
+    {
+      title: "キャラクターを描こう① ラフ～線画",
+      link: "https://youtu.be/M_FlAXcKcY4",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 21,
+    },
+    {
+      title: "キャラクターを描こう② 塗り～パーツ分け",
+      link: "https://youtu.be/_roxvUvK91A",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 18,
+    },
+    {
+      title: "Live2D 取り込みと下準備",
+      link: "https://youtu.be/Cif051pfors",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 19,
+    },
+    {
+      title: "Live2D 動きの設定 コツやテクニックも！",
+      link: "https://youtu.be/aEn0rxj9qDk",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 26,
+    },
+    {
+      title: "Live2D 髪揺れ・胸揺れ 物理演算",
+      link: "https://youtu.be/i4kFxZX0fzE",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 14,
+    },
+    {
+      title: "Live2Dモデルをfacerigで動かす！",
+      link: "https://youtu.be/e5hGPNZdNOA",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 13,
+    },
+    {
+      title: "誰でも簡単に3DVTuberになれる！3Dお披露目＆3D化の技術と費用のお話をします",
+      link: "https://youtu.be/4p3vtuTtufM",
+      mediaType: ["VIDEO"],
+      requiredTime: 24,
+    },
+    {
+      title: "FaceRigで使ってたLive2Dアバターの移行手順を紹介！",
+      link: "https://youtu.be/xfEDI2pmGi8",
+      mediaType: ["USE_MY_PC", "VIDEO"],
+      requiredTime: 11,
+    },
+  ],
+};
