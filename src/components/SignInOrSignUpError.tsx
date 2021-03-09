16f48be84bcdcfdeed74110c3c6f45aa4bb42bde
平田海輝 <seaglory2004@gmail.com>
2021-03-09 17:14:24 +0900

    update: ログインと新規登録時にエラーが出た時に出るエラーメッセージにCSSを追加しました

--- src/components/SignInOrSignUpError.tsx
+++ src/components/SignInOrSignUpError.tsx
@@ -1,26 +1,57 @@
+import styled from "styled-components";
+import media from "styled-media-query";
+
 interface Props {
   signInOrSignUpError: string;
 }
 
+const ErrorMessage = styled.p`
+  margin: 0 auto 32px;
+  width: 100%;
+  max-width: 300px;
+  color: #f13c20;
+  font-size: 10px;
+  ${media.lessThan("medium")`
+    width: 88%;
+    max-width: 224px;
+  `}
+`;
+
 export default function SignInOrSignUpError({ signInOrSignUpError }: Props) {
   return signInOrSignUpError === "" ? (
-    <p></p>
+    <ErrorMessage></ErrorMessage>
   ) : signInOrSignUpError ===
     "There is no user record corresponding to this identifier. The user may have been deleted." ? (
-    <p>
-      メールアドレスは登録されていません。ユーザーが削除されている可能性があります。
-    </p>
+    <ErrorMessage>
+      メールアドレスは登録されていません。
+      <br />
+      ユーザーが削除されている可能性があります。
+    </ErrorMessage>
   ) : signInOrSignUpError === "The email address is badly formatted." ? (
-    <p>メールアドレスの形式が正しくありません。</p>
+    <ErrorMessage>メールアドレスの形式が正しくありません。</ErrorMessage>
   ) : signInOrSignUpError ===
     "The email address is already in use by another account." ? (
-    <p>メールアドレスはすでに別のアカウントで使用されています。</p>
+    <ErrorMessage>
+      メールアドレスはすでに別のアカウントで使用されています。
+    </ErrorMessage>
   ) : signInOrSignUpError ===
     "The password is invalid or the user does not have a password." ? (
-    <p>パスワードが無効であるか、ユーザーがパスワードを持っていません。</p>
+    <ErrorMessage>
+      パスワードが無効であるか、ユーザーがパスワードを持っていません。
+    </ErrorMessage>
   ) : signInOrSignUpError === "Password should be at least 6 characters" ? (
-    <p>パスワードは6文字以上である必要があります。</p>
+    <ErrorMessage>パスワードは6文字以上である必要があります。</ErrorMessage>
+  ) : signInOrSignUpError ===
+    "Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later." ? (
+    <ErrorMessage>
+      多くのログイン試行の失敗により、このアカウントへのアクセスは一時的に無効になっています。
+      <br />
+      <br />
+      パスワードをリセットしてすぐに復元することも、後で再試行することもできます。
+    </ErrorMessage>
   ) : (
-    <p>予期されないエラーが発生した可能性があります。ごめんね。</p>
+    <ErrorMessage>
+      予期されないエラーが発生した可能性があります。ごめんね。
+    </ErrorMessage>
   );
 }
