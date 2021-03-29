15dd91ad26a9eeddd86d2a65bd61c5fb0c4154c3
平田海輝 <seaglory2004@gmail.com>
2021-03-29 11:57:00 +0900

    docs: 次回アップデート？までに実装する予定のない機能が入る場所にに、その旨を伝えるメッセージを表示するためのフォントを追加しました

---
+++ src/styles/fonts/UnimplementedMessage.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const UnimplementedMessage = styled.h2`
+  margin: 0;
+
+  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo,
+    "MS PGothic", sans-serif;
+  font-size: 4vh;
+  color: red;
+  letter-spacing: 0.04em;
+`;
