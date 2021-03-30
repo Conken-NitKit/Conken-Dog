2c1d2fa8587667a730de01f591fc3cd0f1318df6
平田海輝 <seaglory2004@gmail.com>
2021-03-31 08:27:25 +0900

    add: スマホ画面のアカウントページの、アイコン画像差し替えボタンの大枠を追加しました

--- src/components/account-components/AccountIcon.tsx
+++ src/components/account-components/AccountIcon.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import TestIcon from "../../assets/img/icons/test-icon.png";
 
 const Container = styled.div`
+  display: flex;
+
+  position: relative;
+
   margin-top: 4vh;
   margin-bottom: 8vh;
   margin-left: 22vw;
@@ -12,6 +16,8 @@ const Container = styled.div`
   height: 44vw;
   width: 44vw;
 
+  justify-content: center;
+
   overflow: hidden;
 `;
 
@@ -20,10 +26,28 @@ const Icon = styled.img`
   width: 44vw;
 `;
 
+const EditButton = styled.button`
+  position: absolute;
+
+  border: none;
+
+  padding: 0;
+
+  bottom: 0;
+
+  height: 6.5vw;
+  width: 31vw;
+
+  outline: none;
+
+  background-color: rgba(48, 200, 214, 1);
+`;
+
 export default function AccountIcon() {
   return (
     <Container>
       <Icon src={TestIcon} />
+      <EditButton />
     </Container>
   );
 }
