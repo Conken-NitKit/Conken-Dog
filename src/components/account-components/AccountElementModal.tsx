72f9f8586437c0ed8a93abb143bcd8468480f20f
平田海輝 <seaglory2004@gmail.com>
2021-03-30 12:19:08 +0900

    add: フォルダ「account-component」を追加しました, move: 「Account.tsx」、「AccountElementsCard.tsx」、「AccountElementsModal.tsx」を同フォルダに移動しました

--- src/components/account-components/AccountElementModal.tsx
+++ src/components/account-components/AccountElementModal.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import styled from "styled-components";
 import { AccountElementContext } from "../contexts/AcountElementContext";
-import { AccountButtonTitle } from "../styles/fonts/AccountButtonTitle";
+import { AccountButtonTitle } from "../styles/fonts/ButtonTitle";
 import { AccountTitle } from "../styles/fonts/AccountTitle";
 
 const Modal = styled.div<{ isOpen: boolean }>`
