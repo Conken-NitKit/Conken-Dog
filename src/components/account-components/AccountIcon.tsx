354df7bcb28ae4a6cff77287c58907ac12847628
平田海輝 <seaglory2004@gmail.com>
2021-03-31 09:33:24 +0900

    add: スマホ画面のアカウントページのアイコンカードに、アイコンアイコン（「アイコン」のアイコン笑）欄を追加しました

--- src/components/account-components/AccountIcon.tsx
+++ src/components/account-components/AccountIcon.tsx
@@ -5,13 +5,37 @@ import TestIcon from "../../assets/img/icons/test-icon.png";
 const Container = styled.div`
   display: flex;
 
-  position: relative;
-
   margin-top: 4vh;
   margin-bottom: 8vh;
-  margin-left: 22vw;
+  margin-left: 11vw;
 
-  border-radius: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 22vw;
+
+  width: 55vw;
+
+  box-shadow: 0 3px 6px -3px rgba(0, 0, 0, 0.3);
+`;
+
+const IconIconContainer = styled.div`
+  display: flex;
+
+  border-radius: 22vw 0 0 22vw;
+
+  width: 33vw;
+
+  background-color: rgba(48, 200, 214, 1);
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+
+  position: relative;
+
+  margin-left: -22vw;
+
+  border-left: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 22vw;
 
   height: 44vw;
   width: 44vw;
@@ -40,14 +64,17 @@ const EditButton = styled.button`
 
   outline: none;
 
-  background-color: rgba(48, 200, 214, 1);
+  background-color: white;
 `;
 
 export default function AccountIcon() {
   return (
     <Container>
-      <Icon src={TestIcon} />
-      <EditButton />
+      <IconIconContainer />
+      <IconContainer>
+        <Icon src={TestIcon} />
+        <EditButton />
+      </IconContainer>
     </Container>
   );
 }
