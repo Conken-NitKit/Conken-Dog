0e97251bf77bf0c9b829e577cfbdda5e5b3e7307
kototo522 <huyejiben@gmail.com>
2021-04-01 23:09:49 +0900

    add: 画像の貼りつけ

--- src/components/landing/Approach.tsx
+++ src/components/landing/Approach.tsx
@@ -2,6 +2,17 @@ import styled from "styled-components";
 import { TopImages } from "./approach/TopImages";
 import { Mission } from "./approach/Mission";
 
+import kita from "../../assets/img/landing/approach/kita.png";
+import conken from "../../assets/img/landing/approach/Conken.png";
+import maru from "../../assets/img/landing/approach/maru.png";
+import sinkan from "../../assets/img/landing/approach/sinkan.png";
+
+
+const Div = styled.div`
+  position: relative;
+  height: 2350px;
+`;
+
 const TopBackgroundSquare = styled.div`
   position: absolute;
   top: 0px;
@@ -64,10 +75,49 @@ const Login = styled.h3`
   text-shadow: 3px 3px 2px #191970;
 `;
 
+const Kita = styled.img`
+position: absolute;
+left: 8%;
+top:68%;
+width: 250px;
+`;
+
+const Conken = styled.img`
+position: absolute;
+right: 5%;
+top:70%;
+width: 300px;
+`;
+
+const Maru = styled.img`
+position: absolute;
+right: 17%;
+top:87%;
+width: 280px;
+`;
+
+const Sinkan = styled.img`
+position: absolute;
+left: 8%;
+top:83%;
+width: 350px;
+`;
+
+const Moji = styled.h1`
+position: absolute;
+width: 1361px;
+height: 869px;
+right: -20%;
+top: 78%;
+font-family: Gen Jyuu Gothic;
+font-size: 50px;
+`;
+
+
 //上から下
 export const Approach = () => {
   return (
-    <div>
+    <Div>
       <TopBackgroundSquare>
         
         <TopBackgroundCircle />
@@ -78,8 +128,13 @@ export const Approach = () => {
         <CatchCopy>Learn To Play<br/> Than Anywhere</CatchCopy>
 
         <Mission/>
-
       </TopBackgroundSquare>
-    </div>
+
+      <Kita src={kita}/>
+      <Conken src={conken}/>
+      <Maru src={maru}/>
+      <Sinkan src={sinkan}/>
+      <Moji>遊ぶように学ぼう　どこよりも</Moji>
+    </Div>
   );
 };
