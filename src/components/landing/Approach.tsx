a03283e207f65ceeb449592fe2a87060901bd1f8
kototo522 <huyejiben@gmail.com>
2021-04-01 16:30:31 +0900

    add:画像と円の表示追加

--- src/components/landing/Approach.tsx
+++ src/components/landing/Approach.tsx
@@ -1,11 +1,79 @@
+import styled from "styled-components";
+import { ImageTop } from "./TopImages";
+
+const TopBackgroundSquare = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  height: 921px;
+  width: 100%;
+  background-color: #87ceeb;
+`;
+
+const TopBackgroundCircle = styled.div`
+  position: absolute;
+  top: 726px;
+  height: 389px;
+  width: 1440px;
+  left: 0px;
+  border-radius: 50%;
+  background-color: #87ceeb;
+`;
+
+const WhiteCircle = styled.div`
+  position: relative;
+  height: 550px;
+  width: 550px;
+  margin-right: auto;
+  margin-left: auto;
+  border-radius: 50%;
+  top: 926px;
+  background-color: white;
+  z-index: 2;
+`;
+
+
+const BigCircle = styled.div`
+  position: relative;
+  height: 560px;
+  width: 560px;
+  margin-right: auto;
+  margin-left: auto;
+  border-radius: 50%;
+  top: 371px;
+  background-color: #87ceeb;
+`;
+
+const CatchCopy = styled.h1`
+  position: absolute;
+  width: 1340px;
+  height: 434px;
+  left: 88px;
+  top: 140px;
+  font-family: Gen Jyuu Gothic;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 140px;
+  line-height: 238px;
+  text-align: center;
+  text-transform: capitalize;
+  color: #FFFFFF;
+  text-shadow: 10px 10px 4px #191970;
+`;
+
+//上から下
 export const Approach = () => {
   return (
     <div>
-      <h1>
-        learn like play
-        <br />
-        than anywhere
-      </h1>
+      <TopBackgroundSquare>
+        
+        <TopBackgroundCircle />
+        <ImageTop />
+        <CatchCopy>Learn To Play
+          <br/> Than Anywhere</CatchCopy>
+        <WhiteCircle/>
+        <BigCircle />
+      </TopBackgroundSquare>
     </div>
   );
 };
