b8f66263f4d41a979e67828da3fb9bd5d9363d05
平田海輝 <seaglory2004@gmail.com>
2021-04-04 21:02:33 +0900

    add: 背景画像を追加しました

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import styled from "styled-components";
-
-import Settings from "../components/Settings";
 import NavigationBar from "../components/NavigationBar";
-import SettingsLogo from "../assets/img/icons/settings.svg";
+import { ResponsiveWrapper } from "../layouts/ResponsiveWrapper";
+import { Heading2 } from "../styles/fonts/Heading2";
+import { Small } from "../styles/fonts/Small";
 
-const SettingBtn = styled.img`
-  position: absolute;
-  top: 18px;
-  right: 18px;
-  width: 24px;
-  height: 24px;
+const AccountWrapper = styled.div`
+  margin: 20px;
 `;
 
 export default function Account({ history }: RouteComponentProps) {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <SettingBtn src={SettingsLogo} onClick={() => setIsOpen(true)} />
-      <Settings isOpen={isOpen} close={() => setIsOpen(false)} history={history}/>
+    <ResponsiveWrapper>
+      <AccountWrapper>
+        <Heading2>アカウント</Heading2>
+        <Small>みんなのぷろふぃーる！</Small>
+      </AccountWrapper>
       <NavigationBar />
-    </>
+    </ResponsiveWrapper>
   );
 }
