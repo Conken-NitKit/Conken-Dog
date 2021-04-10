2ec80154c3d0f81e39071cd9417644f4a65e43fc
kubo-hide-kun <hide44041@gmail.com>
2021-04-10 19:30:09 +0900

    update: スタイルの調整

--- src/components/landing/WhatIs.tsx
+++ src/components/landing/WhatIs.tsx
@@ -4,54 +4,57 @@ import WhatIspicture from "../../assets/img/landing/WhatIspicture.png";
 
 const WhatIsContainer = styled(Container)`
   text-align: center;
+  margin: 64px auto;
+  @media (max-width: 414px) {
+    margin: 16px auto;
+  }
 `;
 
 const Title = styled.div`
-  
   margin-top: 0.1vw;
   color: #6fd0ee;
-  font-size: 3.5vw;
+  font-size: 2rem;
   width: 100%;
   @media (max-width: 414px) {
-  font-size: 5.3vw;
-}
+    font-size: 5.3vw;
+  }
 `;
 
 const Sentence = styled.p`
-  font-size: 2.5vw;
+  font-size: 1.4rem;
   color: #7e7e7e;
   @media (max-width: 414px) {
-  font-size: 4vw;
-}
+    font-size: 4vw;
+  }
 `;
 
 const WhatIsImg = styled.img`
   height: 25vw;
-  width: 95%;
+  width: 80%;
   object-fit: cover;
   margin-bottom: 1vw;
   border-radius: 8px;
   @media (max-width: 414px) {
-  width: 80vw;
-  height: 40vw;
-  margin: auto auto;
-}
+    width: 90vw;
+    height: 45vw;
+    margin: auto;
+  }
 `;
 
 export const WhatIs = () => {
   return (
     <WhatIsContainer>
-      <Title>
-        コンピュータ研究部ってどんな部活?
-        </Title>
+      <Title>コンピュータ研究部ってどんな部活?</Title>
       <Sentence>
-        コンピュータに関する<br />
-            あらゆることに『挑戦』する部活です。
-        </Sentence>
+        コンピュータに関する
+        <br />
+        あらゆることに『挑戦』する部活です。
+      </Sentence>
       <Sentence>
-        「〇〇をやってみたい！」というあなたの目標、<br />
-            それらを全力でサポートしていきます！
-        </Sentence>
+        「〇〇をやってみたい！」というあなたの目標、
+        <br />
+        それらを全力でサポートしていきます！
+      </Sentence>
       <WhatIsImg src={WhatIspicture} />
     </WhatIsContainer>
   );
