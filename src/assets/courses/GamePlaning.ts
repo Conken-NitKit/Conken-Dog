c408b0ad4ecc6443bf7cb2d01e5118ed6187aa83
kubo-hide-kun <hide44041@gmail.com>
2022-02-03 01:28:40 +0900

    fix: 不要なコンテンツの削除

--- src/assets/courses/GamePlaning.ts
+++ src/assets/courses/GamePlaning.ts
@@ -1,5 +1,4 @@
 import { ICourses } from ".";
-import { GAME_CARRIER } from "../sections/GAME_CARRIER";
 import { LEVEL_DESIGN } from "../sections/LEVEL_DESIGN";
 
 export const GAME_PLANING_COURSE: ICourses = {
@@ -9,5 +8,5 @@ export const GAME_PLANING_COURSE: ICourses = {
   description:
     "このコースでは実際の大手ゲーム会社の開発現場でも利用されているUnityを使ってゲーム開発を行う際に必要な知識を学習します。\n" +
     "本コースではオフラインでのゲームはもちろん、最終的にはオンラインでの対戦ゲームの開発方法までをチュートリアルを通して身につけることができます。",
-  sections: [GAME_CARRIER, LEVEL_DESIGN],
+  sections: [LEVEL_DESIGN],
 };
